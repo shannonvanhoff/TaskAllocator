8a3b7cd9f85ba7599799bc2f833ae01125d95c1a
shannonvanhoff <ashvin.vanhoff@gmail.com>
2025-04-24 22:26:41 +0530

    TaskInjected

---
+++ TaskAllocator.Application/DTOs/UpdateTaskRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskAllocator.Application.DTOs
+{
+    public class UpdateTaskRequest
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public TaskStatus Status { get; set; }
+    }
+}
---
+++ TaskAllocator.Application/Services/Implementations/TaskService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskAllocator.Application.DTOs;
+using TaskAllocator.Application.Services.Interfaces;
+
+namespace TaskAllocator.Application.Services.Implementations
+{
+    public class TaskService : ITaskService
+    {
+        public Task<TaskDto> AssignTaskAsync(Guid taskId, Guid userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TaskDto> AutoAssignTaskAsync(Guid taskId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TaskDto> CreateTaskAsync(CreateTaskRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteTaskAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<TaskDto>> GetAllTasksAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TaskDto> GetTaskByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TaskDto> UpdateTaskAsync(Guid id, UpdateTaskRequest request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
